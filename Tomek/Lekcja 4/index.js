5b1c002de23dff4c49acb788262d76d7179b3791
charon06 <tomasz.grochocki06@gmail.com>
2015-08-23 23:31:41 +0200

    Lekcja 4 - wpolowie

--- Tomek/Lekcja 4/index.js
+++ Tomek/Lekcja 4/index.js
@@ -1,14 +1,9 @@
-function pickColor () {
-	var div = document.getElementsByClassName('color');
-	var color = div.backgroundColor
-		return color;
-}
-color()
+var box = document.getElementsByClassName('box');
+var color = " ";
 
-var box = document.getElementsByClassName('color');
-var color = box[i].backgroundColor;	
-	for (var i = 0; i < 4; i++) {
-		box[i].addEventListener('click', function (){
-		console.log(color);
-		});
+	for (var i = 0; i < 4; i++) {	
+		box[i].addEventListener('click', function() {
+			box[i].style.backgroundColor = color
+	});
 	}
+
