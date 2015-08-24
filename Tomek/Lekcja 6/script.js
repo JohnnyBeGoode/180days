f1cdef76eab602e9e0a2550f064d83c069ec10e2
charon06 <tomasz.grochocki06@gmail.com>
2015-08-24 23:49:06 +0200

    Lekcja 6 - TOMEK

---
+++ Tomek/Lekcja 6/script.js
@@ -0,0 +1,34 @@
+var date = new Date();
+var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+var time = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+var day = days[date.getDay()]
+
+var dayHTML = document.getElementById('day');
+
+var webDays = document.getElementsByClassName('day');
+
+for (var i = 0; i < webDays.length; i++){
+	if (webDays[i].innerHTML === day){
+		webDays[i].style.height = "500px";
+		webDays[i].style.backgroundColor = "#FCA71D";		
+		webDays[i].innerHTML = "";
+		dayHTML.innerHTML = day;
+	}
+}
+
+function realTime() {
+	var date = new Date(); // Dlaczego ta zmienna musi byc w funkcji ? ? ? 
+	var time = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds(); // Dlaczego ta zmianna musi byc w funckji ??????
+	var hourHTML = document.getElementById('hour');
+	hourHTML.innerHTML = time;
+	refresh();
+}
+
+
+function refresh() {
+	setTimeout('realTime()', 1000);  // Dlaczego ta funckja musi byc w ciapkach????
+}
+
+
+
