5abbe38a24ed85a99d720b3f61583e532ac2f114
JohnnyBeGoode <180websites180@gmail.com>
2015-09-01 22:36:34 +0200

    Leckaj 8 Kamil bug

--- Kamil/Lekcja 8/index.js
+++ Kamil/Lekcja 8/index.js
@@ -3,8 +3,11 @@ var textShadowList = document.getElementsByClassName( 'text-shadow' );
 
 var boxBlur = 0;
 var boxShadow = 0;
-var textBlur = 0;
-var shadowPosition = 0;
+var spanBlur = 0;
+var spanShadowPosition = 0;
+var paraBlur = 0;
+var paraShadowPosition = 0;
+var tag = '';
 
 for ( i=0; i < boxShadowList.length; i++ ) {
     boxShadowList[i].addEventListener( 'click', function(){
@@ -14,18 +17,23 @@ for ( i=0; i < boxShadowList.length; i++ ) {
 
 for ( i=0; i < textShadowList.length; i++ ) {
     textShadowList[i].addEventListener( 'click', function(){
-        dropShadow(this, 'text');
+        tag = this.tagName;
+        dropShadow(this, 'text', tag);
     });
 }
 
-function dropShadow(element, type) {
+function dropShadow(element, type, tag) {
     if ( type == 'box' ) {
         boxBlur += 3;
         boxShadow ++;
         element.style.boxShadow = '#ccc 0 0 ' + boxShadow + 'px ' + boxBlur + 'px';
+    } else if ( tag == 'SPAN' ) {
+        spanBlur += 2;
+        spanShadowPosition += 1;
+        element.style.textShadow = '#000 0 ' + spanShadowPosition + 'px ' + spanBlur + 'px';
     } else {
-        textBlur += 2;
-        shadowPosition += 1;
-        element.style.textShadow = '#000 0 ' + shadowPosition + 'px ' + textBlur + 'px';
+        paraBlur += 2;
+        paraShadowPosition += 1;
+        element.style.textShadow = '#000 0 ' + paraShadowPosition + 'px ' + paraBlur + 'px';
     }
 }
