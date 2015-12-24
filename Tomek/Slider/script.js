2033f5c88ebb59c3fdb1f0576852e965e7ac9428
Tomek <charon34512@gmail.com>
2015-12-25 00:07:31 +0100

    Bug Fix

--- Tomek/Slider/script.js
+++ Tomek/Slider/script.js
@@ -1,33 +1,26 @@
 var container = document.querySelector('.container');
 var images = document.querySelectorAll('div.container > div');
 
-// Need to convert Nodelist to Array
-var arr = [];
-function makeArray(nodelitst){
-	for (var i = 0; i < nodelitst.length; i++) {
-		arr.push(nodelitst[i])
-	};
-	return arr;
-}
-makeArray(images)
-arr.reverse()
+// Convert NodeList to Array
+var array = Array.prototype.slice.call(images);
+array.reverse()
 
-// Slider Function
 var slides = images.length - 1
 function slider(){
 	if(slides < 0){
 		slides = images.length - 1
 	}
-	container.removeChild(arr[slides])
-	container.insertBefore(arr[slides], null)	
-	slides--
-	arr.forEach(function(a){
+	array.forEach(function(a){
 		a.style.animation = 'move 2s'
-	})
+	})	
+	container.removeChild(array[slides])
+	container.insertBefore(array[slides], null)	
+	slides--
 	setTimeout(slider, 4000)
 }
+
 function anim(){
-	arr.forEach(function(a){
+	array.forEach(function(a){
 		a.style.animation = ''
 	})	
 	setTimeout(anim, 3980)
